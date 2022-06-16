b88ddf14eee2df3abaa03a904392b50b085ea332
Your Name <you@example.com>
2022-06-16 15:44:25 +0700

    first commit

---
+++ config/koneksi.php
@@ -0,0 +1,17 @@
+<?php
+    $server = "localhost";
+    $username = "id19064280_bagas_cahya";
+    $password = "Bcr_09122001";
+    $database = "id19064280_capstone";
+
+   $conn = mysqli_connect($server, $username, $password, $database);
+  /*  if (mysqli_connect_errno()) {
+    echo "Koneksi gagal: " . mysqli_connect_error();
+    exit();
+  } else {
+      echo "Koneksi success";
+  } */
+?> 
+
+
+
---
+++ modul/tentang.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+   <title>Tentang</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='aset/tentang/style.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+   <link rel="stylesheet" href="aset/login/css/style.css">
+  
+  
+</head>
+<style>
+@media only screen and (max-width: 400px) {
+    img {
+        width: 100%;
+    }
+}
+</style>
+<body>
+
+  <div class="ma3">
+  
+ <img src="aset/banner/Group 14.png" alt=""
+  width="1250" 
+     height="700" />
+
+</div>
+</body>
+
+</html>
